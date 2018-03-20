087f656f0e60b42a9f2e2bb837f332ac119082a6
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-03-20 16:04:12 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,15 @@ class App extends React.Component {
     });
   }
 
+  componentWillMount() {
+    this.fetchTweets()
+    this.startInterval()
+  }
+
+  componentWillUnmount() {
+    this.cleanUpInterval()
+  }
+
   render() {
     return (
       <div>
--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -14,6 +14,23 @@ class TweetWall extends React.Component {
   // TODO: shouldComponentUpdate()
   // TODO: componentWillReceiveProps()
 
+  componentWillReceiveProps(nextProps) {
+    debugger
+    this.setState({
+      tweets: nextProps.newTweets.concat(this.state.tweets)
+    })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.state.tweets.includes(nextProps.newTweets));
+  }
+
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
+
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
 
